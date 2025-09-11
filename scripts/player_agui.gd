fb235db32a0f2764db4d84dd282172ebd37da855
Bfdiego <dayo.alba.p@gmail.com>
2025-09-11 19:19:04 -0400

    Agui animations added

--- scripts/player_agui.gd
+++ scripts/player_agui.gd
@@ -1,32 +1,77 @@
 extends CharacterBody2D
 
 @export var speed: float = 500
-@export var jump_force: float = -500
-
+@export var jump_force: float = -900
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var state_machine = $AnimationTree.get("parameters/playback")
 
+var attacking: bool = false
+var current_attack: String = ""
+var last_state: String = ""
+var in_air: bool = false
+
 func _physics_process(delta: float) -> void:
-	# add gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		state_machine.travel("Jump")
-		
+		if not in_air:
+			in_air = true
+			_play_state("Jump")
+	else:
+		in_air = false
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_force
-		state_machine.travel("Jump")
-		
+		in_air = true
+		_play_state("Jump")
+	
+	if not attacking:
+		if in_air and (Input.is_action_just_pressed("attack1") or Input.is_action_just_pressed("attack2")):
+			_start_attack("Attack3")
+		elif not in_air and Input.is_action_just_pressed("attack1"):
+			_start_attack("Attack1")
+		elif not in_air and Input.is_action_just_pressed("attack2"):
+			_start_attack("Attack2")
+
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * speed
-		state_machine.travel("Walk")
+		
 		if direction < 0:
-			$Sprite2D.scale.x = abs($Sprite2D.scale.x) 
+			$Sprite2D.scale.x = abs($Sprite2D.scale.x)
 		elif direction > 0:
 			$Sprite2D.scale.x = abs($Sprite2D.scale.x) * -1
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-		state_machine.travel("Idle")
-		
+
+	if not attacking:
+		if in_air:
+			_play_state("Jump")
+		else:
+			if direction != 0:
+				_play_state("Run")
+			else:
+				_play_state("Idle")
+
+
 	move_and_slide()
+
+
+
+func _play_state(state: String) -> void:
+	if state != last_state:
+		state_machine.travel(state)
+		last_state = state
+
+func _start_attack(attack_name: String) -> void:
+	attacking = true
+	current_attack = attack_name
+	_play_state(attack_name)
+	await $AnimationTree.animation_finished
+	attacking = false
+	current_attack = ""
+
+func take_hit() -> void:
+	attacking = false
+	current_attack = ""
+	_play_state("Hit")
