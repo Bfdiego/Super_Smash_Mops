42dd277b6bd1b783562344c93247233e808c7e3c
Bfdiego <dayo.alba.p@gmail.com>
2025-09-15 22:23:35 -0400

    Feat: Flow scenes

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,10 +4,10 @@ extends Node2D
 
 func _on_next_scene() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu/mode_selection.tscn")
+
+func _process(delta: float) -> void:
+	var skip_p1 = Input.is_action_just_pressed("ok_p1")
+	var skip_p2 = Input.is_action_just_pressed("ok_p2")
 	
-#Hovering
-func _ready() -> void:
-	var skip_p1 = Input.is_action_pressed("ok_p1")
-	var skip_p2 = Input.is_action_pressed("ok_p2")
-	if(skip_p1 || skip_p2):
+	if skip_p1 or skip_p2:
 		_on_next_scene()
